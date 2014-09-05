2dade8aeb9bd0ff4abcd42f0162e245aa0205802
Nicolas Garnier <nivco.las@gmail.com>
2014-09-05 16:18:54 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,58 @@
+#Dart Pub packages dependency finder
+
+This is a command line tool that will help you find all packages in Pub that have a given dependency.
+
+#Usage
+
+This script is written in Dart so you'll need Dart installed. You run it using the following command:
+
+    dart dependency_finder.dart <package_name> [--skip-downloads]
+    
+Where:
+
+ - `<package_name>` Is the name if the dependency package you are looking for dependents of.
+ - `--skip-downloads` This will skip downloading the latest version of all packages. You can use that on subsequent searches to avoid re-dowanloading everything which is slow.
+
+##Example usage
+
+This is an example we'll explain later:
+
+    dart dependency_finder.dart js
+    
+This will list all packages in Dart Pub that have a dependency on the `package:js` library in Pub. We do that by:
+
+ - Fetching the list of all existing packages
+ - Looking for what is their latest version
+ - Downloading the latest version of each Pub packages archive
+ - Extracting the `pubspec.yaml` file out of all package archives
+ - Looking for the `js` dependency
+
+In this particular example the output would be:
+
+    computername:bin username$ dart dependency_finder.dart js
+    Listing all packages... 1181 - Done.
+    Finding last versions of each packages... 1181/1181 - Done.
+    Deleting existing .\pub_packages_dl directory.
+    Downloading packages... 1181/1181 - Done.
+    Analyzing packages... 1181/1181
+    All Done! Found 75 packages with packages:js dependency:
+     - add_v1_api-0.1.0
+     - aloha-2.0.0
+     - dancer-0.4.0+4
+     - dartwork-0.1.0
+     - diffbot-0.1.1
+     - form_elements-1.1.0
+     - google_adexchangeseller_v1_api-0.4.9
+     - google_adsense_v1_1_api-0.3.8
+     - google_adsense_v1_2_api-0.4.9
+     - google_adsense_v1_api-0.3.8
+     - google_adsensehost_v4_1_api-0.4.9
+     - google_analytics_v2_4_api-0.4.9
+     - google_analytics_v3_api-0.4.9
+     - google_androidpublisher_v1_api-0.4.9
+     - google_audit_v1_api-0.4.9
+     - google_bigquery_v2_api-0.4.9
+     - google_blogger_v2_api-0.4.9
+     - google_blogger_v3_api-0.4.9
+     - google_books_v1_api-0.4.9
+     ...
---
+++ bin/dependency_finder.dart
@@ -0,0 +1,287 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
+import 'package:yaml/yaml.dart';
+
+// Template of the URL to fetch the list fo existing packages in Pub.
+final String packagesListUrlTemplate = "https://pub.dartlang.org/packages.json?page={pageIndex}";
+// Folder name where the packages will be dowloaded and extracted.
+final String packageDownloadFolder = "pub_packages_dl";
+// Template of the URL to download the package archives.
+final String packageDownloadTemplate = "https://storage.googleapis.com/pub.dartlang.org/packages/{name}-{version}.tar.gz";
+
+// List of all packages where we found the given dependency.
+List<String> packagesWithDependency = new List<String>();
+// Errors encountered during the analysis of packages.
+List<String> errors = new List<String>();
+// Name of the package dependency to look for.
+String packageDependencyToFind;
+// The list of all URLs of description files for all existing packages in pub.
+List<String> packagesDescriptionFileUrls = new List<String>();
+// Maps of last versions of package names.
+Map<String, String> packagesVersions = new Map<String, String>();
+
+
+/**
+ * This is a comand line script that will download the latest version of all packages in pub and
+ * check if they have the given dependency.
+ * Usage: dart dependency_finder.dart <package_name> [--skip-downloads]
+ *
+ * <package_name>: name of the package dependency we'll look for in the pubspec.yaml files.
+ * --skip-downloads: if you already have downloaded all packages (which is the slow part) this will
+ *                   jump directly to the extraction/analysis process.
+ */
+void main(List<String> args) {
+
+  // If no arguments were pased we display a help message.
+  if (args.length == 0) {
+    stdout.writeln("This script will download the latest version of all packages in pub and check if they have the given dependency.");
+    stdout.writeln("Usage: dart dependency_finder.dart <package_name> [--skip-downloads]");
+    return;
+  }
+
+  // Extracing first arg.
+  stdout.write("Listing all packages... ");
+  packageDependencyToFind = args[0];
+
+  // We ump to step 4 if --skip-downloads is used. Otherwise Step 1.
+  if (args.contains("--skip-downloads")) {
+    _analyzeNextPackage();
+  }else {
+    _getPackages();
+  }
+}
+
+// Step 1 - We get the list of all packages description files.
+void _getPackages({int packagesListIndex : 1}) {
+
+  // Download and readsn the next page of list of packages.
+  String url = packagesListUrlTemplate.replaceFirst("{pageIndex}", "$packagesListIndex");
+  get(url).then((Response response) {
+
+    // Handle download errors.
+    if (response.statusCode < 200 || response.statusCode > 299) {
+      stderr.writeln("\nError reading URL $url: Exit with status code: ${response.statusCode} - reason: ${response.reasonPhrase}");
+      return;
+    }
+
+    // Reads the content of the fil as JSON to extract the list of packages descriptors.
+    var json = JSON.decode(response.body);
+    packagesDescriptionFileUrls.addAll(json["packages"]);
+    stdout.write("\rListing all packages... ${packagesDescriptionFileUrls.length}");
+
+    // Move on to downloading the next page of packages listing.
+    if (json["packages"].length != 0 && json["next"] != null) {
+      _getPackages(packagesListIndex: packagesListIndex + 1);
+
+    // We reached the last page so we move on to the next step.
+    } else {
+      stdout.writeln(" - Done.");
+      stdout.write("Finding last versions of each packages... 0/${packagesDescriptionFileUrls.length}");
+      _getNextPackageVersions();
+    }
+
+  // Handle download errors.
+  }).catchError((error){
+    stderr.write("\nError while reading URL $url: $error\n");
+    return true;
+  });
+}
+
+// Step 2 - For each package we fetch what is their last version.
+void _getNextPackageVersions() {
+
+  // If we are done reading all package description files.
+  if (packagesDescriptionFileUrls.isEmpty) {
+    stdout.writeln(" - Done.");
+
+    // Delete potentially existing repo and creating empty new one
+    Directory existingDir = new Directory(packageDownloadFolder);
+    if (existingDir.existsSync()) {
+      stdout.writeln("Deleting existing .\\$packageDownloadFolder directory.");
+      existingDir.deleteSync(recursive: true);
+    }
+    new Directory(packageDownloadFolder).createSync();
+
+    // Start the download of all packages.
+    stdout.write("Downloading packages... 0/${packagesVersions.length}");
+    _downloadNextPackage();
+    return;
+  }
+
+  // Download and read the next package description file to find the latest version of the package.
+  String packageDescrUrl = packagesDescriptionFileUrls.removeLast();
+  get(packageDescrUrl).then((Response response) {
+
+    // Handle download errors.
+    if (response.statusCode < 200 && response.statusCode > 299) {
+      stderr.writeln("\nError reading URL $packageDescrUrl: Exit with status code: ${response.statusCode} - reason: ${response.reasonPhrase}");
+      _getNextPackageVersions();
+      return;
+    }
+
+    // Extract last version from JSON body.
+    var json = JSON.decode(response.body);
+    String lastVersion = json["versions"][json["versions"].length - 1];
+
+    // We have the last version number but we need to clean it up to remove heading 0s as the download link won't have heading 0s.
+    List<String> cleanedLastVersion = new List<String>();
+    lastVersion.split(".").forEach((String subVersion){
+        try{subVersion = "${int.parse(subVersion)}";}catch(_){}
+        cleanedLastVersion.add("$subVersion");
+      });
+    lastVersion = cleanedLastVersion.join(".");
+
+    // Save last version in Map and move to next package.
+    String name = json["name"];
+    packagesVersions[name] = lastVersion;
+    stdout.write("\rFinding last versions of each packages... ${packagesVersions.length}/${packagesDescriptionFileUrls.length + packagesVersions.length}");
+    _getNextPackageVersions();
+
+  // Handle download errors
+  }).catchError((error){
+    stderr.writeln("\nError while reading URL $packageDescrUrl: $error");
+    _getNextPackageVersions();
+    return true;
+  });
+}
+
+// Step 3 - We download each packages archives.
+void _downloadNextPackage() {
+  Iterable<String> packageNames = packagesVersions.keys;
+
+  // If we are done downloading all packages archives.
+  if (packageNames.isEmpty) {
+    stdout.write(" - Done.\n");
+
+    // Start extacting and analyzing all packages
+    stdout.write("Analyzing packages... 0/${new Directory(packageDownloadFolder).listSync().length}");
+    _analyzeNextPackage();
+    return;
+  }
+
+  // Download the next package archive in the queue.
+  String packageName = packageNames.first;
+  String packageVersion = packagesVersions[packageName];
+  packagesVersions.remove(packageName);
+  String packageUrl = packageDownloadTemplate.replaceFirst("{name}", packageName).replaceFirst("{version}", packageVersion);
+  get(packageUrl).then((response) {
+
+    // Handle download errors.
+    if (response.statusCode < 200 && response.statusCode > 299) {
+      stderr.writeln("\nError reading URL $packageUrl: Exit with status code: ${response.statusCode} - reason: ${response.reasonPhrase}");
+      _downloadNextPackage();
+      return;
+    }
+
+    // Write the output to a file in the [packageDownloadFolder] folder and move to download next file.
+    new File("$packageDownloadFolder/$packageName-$packageVersion.tar.gz")..createSync()..writeAsBytes(response.bodyBytes).whenComplete((){
+      int downloadedPackages = new Directory(packageDownloadFolder).listSync().length;
+      stdout.write("\rDownloading packages... $downloadedPackages/${packagesVersions.length + downloadedPackages}");
+      _downloadNextPackage();
+
+    // Handle file creation/write errors
+    }).catchError((error){
+      stderr.writeln("\nError while writig to file $packageDownloadFolder/$packageName-$packageVersion.tar.gz: $error");
+      _downloadNextPackage();
+      return true;
+    });
+
+  // Handle download errors
+  }).catchError((error){
+    stderr.writeln("\nError while reading URL $packageUrl: $error");
+    _downloadNextPackage();
+    return true;
+  });
+}
+
+// Step 4 - Extracting the pubspec.yaml fro the packages files
+void _analyzeNextPackage({Iterator<FileSystemEntity> iter, int index : 1}) {
+  // Initializaion if this is first iteration.
+  List<FileSystemEntity> tars = new Directory(packageDownloadFolder).listSync();
+  if (iter == null) {
+    iter = tars.iterator;
+  }
+
+  // If we are done extracting and analysing all packages.
+  if(!iter.moveNext()) {
+
+    // No packages found
+    if(packagesWithDependency.length == 0) {
+      stdout.writeln("\nAll Done! No packages with package:$packageDependencyToFind dependency found!");
+
+    // Packages with the searched for dependency found.
+    } else {
+      stdout.writeln("\nAll Done! Found ${packagesWithDependency.length} packages with packages:$packageDependencyToFind dependency:");
+      packagesWithDependency.forEach((String path) {
+        stdout.writeln(" - $path");
+      });
+    }
+
+    // Dispay errors encountered during analyze.
+    if(errors.length != 0) {
+      stdout.writeln("\nThere was also ${errors.length} errors while analyzing packages:");
+      errors.forEach((String path) {
+        stdout.writeln(" - $path");
+      });
+    }
+    return;
+  }
+
+  // Extract and Analyze the next ackage archive.
+  FileSystemEntity file = iter.current;
+  extractPubSpecFromTarGz(file.path).then((String yaml){
+
+    // Error in case pubspec.yaml content returned as empty or null.
+    if (yaml == null || yaml == "") {
+      errors.add("${file.path}/pubspec.yaml is empty or null.");
+      _analyzeNextPackage(iter: iter, index: index + 1);
+      return;
+    }
+
+    // Parse the pubspec.yaml as Yaml content.
+    var pubspecYaml;
+    try {
+      pubspecYaml = loadYaml(yaml);
+    } catch (error) {
+      errors.add("Problem parsing Yaml file of ${file.path}/pubspec.yaml: $error");
+      _analyzeNextPackage(iter: iter, index: index + 1);
+      return;
+    }
+
+    // Look for the searched for dependency in the pubspec.yaml's dependecies
+    if(pubspecYaml["dependencies"] != null && pubspecYaml["dependencies"][packageDependencyToFind] != null) {
+      packagesWithDependency.add(file.path.replaceFirst("$packageDownloadFolder/", "").replaceFirst(".tar.gz", ""));
+    }
+
+    // Move on to analyzing the next package.
+    stdout.write("\rAnalyzing packages... $index/${tars.length}");
+    _analyzeNextPackage(iter: iter, index: index + 1);
+
+  // Handle Extraction errors.
+  }).catchError((error){
+    errors.add("There was an error extracting ${file.path}: $error");
+    _analyzeNextPackage(iter: iter, index: index + 1);
+    return true;
+  });
+}
+
+/**
+ * Extracts the pubspec.yaml file as a [String] from the targz file located at [filePath].
+ */
+Future<String> extractPubSpecFromTarGz(String filePath) {
+  var completer = new Completer<String>();
+
+  // Run tar from command line.
+  var processFuture = Process.run("tar",
+      ["--extract", "--gunzip", "--file", filePath, "--to-stdout", "pubspec.yaml"]);
+  processFuture.then((ProcessResult result) {
+    completer.complete(result.stdout);
+  }).catchError((error) {
+    completer.completeError(error);
+    return true;
+  });
+
+  return completer.future;
+}
